471d90c340cae04b03073f3059af4a55b5457a26
Marghub Akhtar <marghub@pluto7.com>
2020-06-29 13:39:54 +0530

    Main.py file added

---
+++ test.py
@@ -0,0 +1,22 @@
+import pandas as pd
+impotr numpy as np
+import scipy.stats as stats
+import pandas_profiling
+from sklearn.metrics import mean_squared_error
+%matplotlib inline
+
+
+train =  pd.read_csv('/input/train.csv')
+test = pd.read_csv('/input/test.csv')
+
+model = LinearRegression(train).fit()
+model.predict(test)
+
+
+
+print("Modelling is done successfully")
+
+print('Happy Coding')
+
+
+
